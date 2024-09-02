93813fb9a2c13088d44aca1c1870526d164113f4
unknown <kaito.stenroos@outlook.com>
2024-09-02 23:58:39 +0300

    First Commit

---
+++ src/main/java/bookstore/bookstore/domain/book.java
@@ -0,0 +1,10 @@
+package bookstore.bookstore.domain;
+
+public class book {
+    private String title;
+    private String author;
+    private int year;
+    private String isbn;
+    private double price;
+
+}
---
+++ src/main/java/bookstore/bookstore/web/bookcontroller.java
@@ -0,0 +1,19 @@
+package bookstore.bookstore.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class bookcontroller {
+
+    @RequestMapping(value="/index", method=RequestMethod.GET)
+    public String Books(Model model) {
+        return "";
+    }
+
+
+
+}
