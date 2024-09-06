59d7b3781262cdbb6505a970c5e0aaaf99b790e0
unknown <kaito.stenroos@outlook.com>
2024-09-06 16:05:46 +0300

    h2db added

--- src/main/java/bookstore/bookstore/domain/Kirja.java
+++ src/main/java/bookstore/bookstore/domain/Kirja.java
@@ -1,9 +1,15 @@
 package bookstore.bookstore.domain;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class Kirja {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private String id;
     private String title;
     private String author;
     private int year;
