ee593498b33e541db0d68d37e751241799a59f7a
unknown <kaito.stenroos@outlook.com>
2024-10-02 19:23:05 +0300

    Tests added

--- src/main/java/bookstore/bookstore/WebSecurityConfig.java
+++ src/main/java/bookstore/bookstore/WebSecurityConfig.java
@@ -6,18 +6,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import java.util.List;
-import java.util.ArrayList;
 
 import bookstore.bookstore.web.UserDetailServiceImpl;
 
--- src/test/java/bookstore/bookstore/BookstoreApplicationTests.java
+++ src/test/java/bookstore/bookstore/BookstoreApplicationTests.java
@@ -1,13 +1,53 @@
 package bookstore.bookstore;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import java.util.List;
+
+import bookstore.bookstore.web.KirjaController;
+import bookstore.bookstore.domain.Kirja;
+import bookstore.bookstore.domain.Category;
+import bookstore.bookstore.domain.BookRepository;
+import bookstore.bookstore.domain.CategoryRepository;
 
 @SpringBootTest
 class BookstoreApplicationTests {
 
+	@Autowired
+	private KirjaController controller;
+
+	@Autowired
+	private BookRepository repository;
+
+	@Autowired
+	private CategoryRepository cRepository;
+
 	@Test
-	void contextLoads() {
+	public void contextLoads() throws Exception {
+		assertThat(controller).isNotNull();
 	}
+
+	@Test
+	void searchBookByIsbn() {
+		List<Kirja> books = repository.findByIsbn("2212343-5");
+		assertThat(books).hasSize(1);
+		assertThat(books.get(0).getTitle()).isEqualTo("Animal Farm");
+	}
+
+	@Test
+	void createNewBook() {
+		Kirja kirja = repository.save(new Kirja("山田鐘人, アベツカ","葬送のフリーレン 8", 2022, "978-4-09-851148-8", 500.00, cRepository.findByName("Nonfiction").get(0)));
+		assertThat(kirja.getId()).isNotNull();
+	}
+
+	@Test
+	void deleteBook() {
+		Long bookId = 1L;
+		repository.deleteById(bookId);
+	}
+
 
 }
